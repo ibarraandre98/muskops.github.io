7d8a90ae8b9f673c5e941756c1861276c1f31c90
Andre Ibarra Perez <RayAsm1@hotmail.com>
2020-12-06 02:26:30 -0600

    Fix de errores finales

--- js/script.js
+++ js/script.js
@@ -5,35 +5,42 @@ $('#to-inicio').on('click', function(e) {
     }, 600);
 });
 
+$('#to-inicio1').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $('#inicio').offset().top
+    }, 600);
+});
+
 $('#to-yo').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
-        scrollTop: $('#acerca').offset().top + 5
+        scrollTop: $('#acerca').offset().top - 35
     }, 600);
 });
 
 $('#to-proyecto').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
-        scrollTop: $('#proyecto').offset().top + 5
+        scrollTop: $('#proyecto').offset().top - 60
     }, 600);
 });
 
 $('#to-logros').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
-        scrollTop: $('#logros').offset().top + 5
+        scrollTop: $('#logros').offset().top - 60
     }, 600);
 });
 
 $('#to-contacto').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
-        scrollTop: $('#contacto').offset().top + 5
+        scrollTop: $('#contacto').offset().top
     }, 600);
 });
 
-$('#to-inicio1').on('click', function(e) {
+/* $('#to-inicio1').on('click', function(e) {
     $('li').removeClass('active');
     $('#to-inicio1').addClass('active');
 });
@@ -56,4 +63,38 @@ $('#to-logros').on('click', function(e) {
 $('#to-contacto').on('click', function(e) {
     $('li').removeClass('active');
     $('#to-contacto').addClass('active');
+}); */
+
+$(window).bind('scroll', function() {
+    if ($('#sm-navv').offset().top < 100) {
+        $('#sm-navv').addClass("sm-ante-nav-saved");
+    } else {
+        $('#sm-navv').removeClass("sm-ante-nav-saved");
+        $('#sm-navv').addClass('sm-ante-nav');
+    }
+
+    if ($('#sm-navv').offset().top > $('#inicio').offset().top) {
+        $('li').removeClass('active');
+        $('#to-inicio1').addClass('active');
+    }
+
+    if ($('#sm-navv').offset().top > $('#acerca').offset().top - 100) {
+        $('li').removeClass('active');
+        $('#to-yo').addClass('active');
+    }
+
+    if ($('#sm-navv').offset().top > $('#proyecto').offset().top - 100) {
+        $('li').removeClass('active');
+        $('#to-proyecto').addClass('active');
+    }
+
+    if ($('#sm-navv').offset().top > $('#logros').offset().top - 100) {
+        $('li').removeClass('active');
+        $('#to-logros').addClass('active');
+    }
+
+    if ($('#sm-navv').offset().top > $('#contacto').offset().top - 200) {
+        $('li').removeClass('active');
+        $('#to-contacto').addClass('active');
+    }
 });
